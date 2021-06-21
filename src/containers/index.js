defa545c90670f449f65b2303dd1a0693810af0b
Elena <78875237+ElenaHellen@users.noreply.github.com>
2021-06-21 11:28:11 +0200

    Update and rename RegisterContainer.js to index.js

---
+++ src/containers/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContactForm from "../../components/login/loginPage"
+
+class ContactPage extends React.Component {
+  
+  render() {
+    return <ContactForm onSubmit={this.submit} />
+  }
+}
+export default ContactPage
